e30ead2e705ad5cc70e0c2757ee6080502d3eddf
jhonsreis <jhonsreis@gmail.com>
2023-05-23 18:15:19 -0300

    🐣

---
+++ src/components/Timer.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class Timer extends Component {
+  state = {
+    timer: 30,
+    timerHandler: '',
+  };
+
+  componentDidMount() {
+    const oneSecond = 1000;
+    this.setState({
+      timer: 30,
+      timerHandler: setInterval(this.decreaseTimer, oneSecond),
+    });
+  }
+
+  decreaseTimer = () => {
+    const { timer, timerHandler } = this.state;
+    const { timeOut } = this.props;
+    if (timer === 0) {
+      clearInterval(timerHandler);
+      return timeOut();
+    }
+    this.setState({ timer: timer - 1 });
+  };
+
+  render() {
+    const { timer } = this.state;
+    return (
+      <div>
+        <p>{timer}</p>
+      </div>
+    );
+  }
+}
+
+Timer.propTypes = {
+  timeOut: PropTypes.func,
+}.isRequired;
