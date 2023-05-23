cde46f2c416caf33fd569ad144afba3274540201
Ronaldo Cerenza <rockronald@gmail.com>
2023-05-23 15:18:04 -0400

    req6: ainda em andamento

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,29 +1,14 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Header from '../components/Header';
-import { getQuestions } from '../helpers/triviaAPI';
+import Question from '../components/Question';
 
 export default class Game extends Component {
-  async componentDidMount() {
-    const token = localStorage.getItem('token');
-    const APIdata = await getQuestions(token);
-    if (APIdata) {
-      console.log(APIdata);
-    } else {
-      this.logout();
-    }
-  }
-
-  logout = () => {
-    const { history } = this.props;
-    localStorage.setItem('token', '');
-    history.push('/');
-  };
-
   render() {
     return (
       <div>
         <Header />
+        <Question />
       </div>
     );
   }
